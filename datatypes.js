04be89e7342bd0b93d04cb0fa13ff67b5607ca9b
sumit <saggarwal@innow8apps.com>
2024-08-27 15:36:45 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+
+"use strict";               // Treat our js as newer js version 
+
+// alert(3+3)               // we are using node js not browser
+
+// datatypes
+let name = "Sumit"      // string
+let age = 20            // number 2 to power 53
+let isLoggedIn = false  // boolean
+
+
+// bigint
+// undefined   // value is not assigned yet
+// null        // it is a standalone value or empty value
+// symbol      // unique
+// object 
+
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
